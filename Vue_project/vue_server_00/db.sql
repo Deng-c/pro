ff0c54a6d96a28c0321ffc53d1f4abab4476c8a8
Deng-c <deng176869@163.com>
2019-07-15 20:58:21 +0800

    第二次提交

--- Vue_project/vue_server_00/db.sql
+++ Vue_project/vue_server_00/db.sql
@@ -1,8 +1,12 @@
+#连接utf8
 SET NAMES UTF8;
+#删除数据库
 DROP DATABASE IF EXISTS msj;
+#创建数据库
 CREATE DATABASE msj CHARSET=UTF8;
+#进入数据库
 USE msj;
-
+#创建登录表
 CREATE TABLE msj_login(
   id INT PRIMARY KEY AUTO_INCREMENT,
   uname VARCHAR(50),
@@ -25,6 +29,26 @@ INSERT INTO msj_laptop_carousel VALUES(null,'a-5.jpg',"西瓜冰粥",'富含氨�
 INSERT INTO msj_laptop_carousel VALUES(null,'a-6.jpg',"哈密瓜雪糕",'生津止渴，补充维生素',6);
 
 
-#以下代码创建购物车表与添加测试数据
-
-
+#主页2L商品
+CREATE TABLE msj_laptop(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  img_url VARCHAR(128),
+  title VARCHAR(128),
+  subtitle VARCHAR(128),
+  img_uname VARCHAR(128),
+  uname VARCHAR(128)
+);
+INSERT INTO msj_laptop VALUES(null,"01.jpg","小米山药粥","热门 | 最受欢迎的美食","011.jpg","elsa飞雪");
+INSERT INTO msj_laptop VALUES(null,"02.jpg","奥尔良烧烤味肉饼","热门 | 吃起来慢慢的幸福感","022.jpg","寓言7656");
+INSERT INTO msj_laptop VALUES(null,"03.jpg","甜了个甜甜圈",null,"033.jpg","Momo的食摄日记");
+INSERT INTO msj_laptop VALUES(null,"04.jpg","香焖猪尾巴","家常菜 | 天涯海角最念得味道","044.jpg","美美家的厨房");
+INSERT INTO msj_laptop VALUES(null,"05.jpg","辣根温拌秋葵毛蛤",null,"055.jpg","踏月色而来～");
+INSERT INTO msj_laptop VALUES(null,"06.jpg","烙菜馍","热门 | 最受欢迎的美食","022.jpg","寓言7656");
+INSERT INTO msj_laptop VALUES(null,"07.jpg","红烧冰糖猪手",null,"077.jpg","18LS28xgl零落");
+INSERT INTO msj_laptop VALUES(null,"08.jpg","核桃糊","热门 | 宝宝营养辅食","088.png","宝宝营养辅食");
+INSERT INTO msj_laptop VALUES(null,"09.jpg","肉松皮蛋豆腐","家常菜 | 经典的家常小菜","099.jpg","meggy跳舞的苹果");
+INSERT INTO msj_laptop VALUES(null,"10.jpg","自制凉粉","热门 | 冰凉爽滑","1010.jpg","馋.猫");
+INSERT INTO msj_laptop VALUES(null,"11.jpg","西芹虾仁","降压 | 降压好菜","1111.png","原味生活家");
+INSERT INTO msj_laptop VALUES(null,"12.jpg","腊肠土豆脱糖饭","热门 | 美容养颜，助消化","1212.jpg","风意画");
+INSERT INTO msj_laptop VALUES(null,"13.jpg","自制杨梅干",null,"077.jpg","孔老师教做菜");
+INSERT INTO msj_laptop VALUES(null,"14.jpg","上汤西兰花","家常菜 | 最受欢迎的美食","044.jpg","美美家的厨房");
